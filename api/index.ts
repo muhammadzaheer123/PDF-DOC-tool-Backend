e65f6e0ed612f1cd1b37bce4ec96a7feb287f27a
muhammadzaheer <zaheerkhan62145@gmail.com>
2026-08-05 23:04:20 +0500

    fix: point serverless entry to compiled dist output instead of src

--- api/index.ts
+++ api/index.ts
@@ -4,11 +4,11 @@ import { ConfigService } from "@nestjs/config";
 import { ValidationPipe, INestApplication } from "@nestjs/common";
 import express, { Request, Response } from "express";
 import helmet from "helmet";
-import { AppModule } from "../src/app.module";
-import { AllExceptionsFilter } from "../src/common/filters/all-exceptions.filter";
-import { ResponseInterceptor } from "../src/common/response/response.interceptor";
-import { sanitizeRequestBody } from "../src/common/middleware/sanitize-request-body.middleware";
-import { AppConfig } from "../src/config/configuration";
+import { AppModule } from "../dist/src/app.module";
+import { AllExceptionsFilter } from "../dist/src/common/filters/all-exceptions.filter";
+import { ResponseInterceptor } from "../dist/src/common/response/response.interceptor";
+import { sanitizeRequestBody } from "../dist/src/common/middleware/sanitize-request-body.middleware";
+import { AppConfig } from "../dist/src/config/configuration";
 
 const server = express();
 let cachedApp: INestApplication | null = null;
